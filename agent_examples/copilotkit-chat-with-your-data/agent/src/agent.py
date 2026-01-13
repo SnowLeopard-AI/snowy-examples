a493e817551c65c7937d8e0019c248638523b608
Luke Lalor <luke@snowleopard.ai>
2026-01-13 10:20:34 -0800

    data requests working now

--- agent_examples/copilotkit-chat-with-your-data/agent/src/agent.py
+++ agent_examples/copilotkit-chat-with-your-data/agent/src/agent.py
@@ -1,12 +1,17 @@
+import os
 from textwrap import dedent
+
+from ag_ui.core import EventType, StateSnapshotEvent
+# load environment variables
+from dotenv import load_dotenv
 from pydantic import BaseModel, Field
 from pydantic_ai import Agent, RunContext
 from pydantic_ai.ag_ui import StateDeps
-from ag_ui.core import EventType, StateSnapshotEvent
+from pydantic_ai.messages import ToolReturn
 from pydantic_ai.models.openai import OpenAIResponsesModel
+from snowleopard import SnowLeopardClient
+from snowleopard.models import RetrieveResponseError, ErrorSchemaData, SchemaData
 
-# load environment variables
-from dotenv import load_dotenv
 load_dotenv()
 
 # =====
@@ -14,9 +19,9 @@ load_dotenv()
 # =====
 class ProverbsState(BaseModel):
   """List of the proverbs being written."""
-  proverbs: list[str] = Field(
-    default_factory=list,
-    description='The list of already written proverbs',
+  data_responses: dict[str, SchemaData] = Field(
+    default_factory=dict,
+    description='Successful data queries',
   )
 
 # =====
@@ -26,13 +31,12 @@ agent = Agent(
   model = OpenAIResponsesModel('gpt-5-mini'),
   deps_type=StateDeps[ProverbsState],
   system_prompt=dedent("""
-    You are a helpful assistant that helps manage and discuss proverbs.
+    You are a helpful assistant that helps manage and analyze sales data.
     
-    The user has a list of proverbs that you can help them manage.
-    You have tools available to add, set, or retrieve proverbs from the list.
+    Use your tools to perform queries on the user's behalf. 
+    The user will be able to see the entire data object returned and you will receive a preview 
     
-    When discussing proverbs, ALWAYS use the get_proverbs tool to see the current list before
-    mentioning, updating, or discussing proverbs with the user.
+    NEVER assume data schema when making a query. Do not reference specific tables / columns unless you have already seen them in a successful response
   """).strip()
 )
 
@@ -45,23 +49,62 @@ def get_proverbs(ctx: RunContext[StateDeps[ProverbsState]]) -> list[str]:
   print(f"📖 Getting proverbs: {ctx.deps.state.proverbs}")
   return ctx.deps.state.proverbs
 
-@agent.tool
-async def add_proverbs(ctx: RunContext[StateDeps[ProverbsState]], proverbs: list[str]) -> StateSnapshotEvent:
-  ctx.deps.state.proverbs.extend(proverbs)
-  return StateSnapshotEvent(
-    type=EventType.STATE_SNAPSHOT,
-    snapshot=ctx.deps.state,
-  )
 
 @agent.tool
-async def set_proverbs(ctx: RunContext[StateDeps[ProverbsState]], proverbs: list[str]) -> StateSnapshotEvent:
-  ctx.deps.state.proverbs = proverbs
-  return StateSnapshotEvent(
-    type=EventType.STATE_SNAPSHOT,
-    snapshot=ctx.deps.state,
+def get_data(ctx: RunContext[StateDeps[ProverbsState]], human_query: str):
+  """Retrieve data from "Northwind" dataset with natural language queries.
+  This dataset includes information about orders, product categories, customer demographics, orders, employees, and geographic regions.
+  You can use this data to create dashboards that provide insights into sales performance, customer behavior, shipping efficiency, and supplier contributions.
+
+  When asking questions, infer user intent and be specific and ask in natural language without making any assumptions on DB structure or schema.
+
+  Example: {"human_query": "Which customers have placed the highest number of orders? Provide the top 20 customers by order count, include
+  customer id, company name, and number of orders, sorted descending by number of orders."}"""
+
+  print(f"📊 Getting Data: \"{human_query}\"")
+  response = SnowLeopardClient().retrieve(
+    user_query=human_query,
+    datafile_id=(os.environ['SNOWLEOPARD_DATAFILE_ID']),
   )
+  if isinstance(response, RetrieveResponseError):
+    print(f"📊 Response Error")
+    return f"{response.responseStatus}: {response.description}"
+  elif isinstance(response.data[-1], ErrorSchemaData):
+    print(f"📊 Data Retrieval Error")
+    data = response.data[-1]
+    rtn = [f"query: {data.query}", f"error: {data.error}"]
+    if data.datastoreExceptionInfo:
+      rtn.append(f"exception_info: {data.datastoreExceptionInfo}")
+    return "\n".join(rtn)
+  else:
+    print(f"📊 Data Retrieval Success")
+    data = response.data[-1]
+    ctx.deps.state.data_responses[ctx.tool_call_id] = data
+    return ToolReturn(
+      return_value=dict(
+        sql_query=data.query,
+        data_top=data.rows[:5],
+        num_rows=len(data.rows),
+      ),
+      metadata=[
+        StateSnapshotEvent(
+          type=EventType.STATE_SNAPSHOT,
+          snapshot=ctx.deps.state,
+        ),
+      ],
+    )
 
 @agent.tool
-def get_weather(_: RunContext[StateDeps[ProverbsState]], location: str) -> str:
-  """Get the weather for a given location. Ensure location is fully spelled out."""
-  return f"The weather in {location} is sunny."
+def read_get_data_response(ctx: RunContext[StateDeps[ProverbsState]], tool_call_id: str, start_row: int = 0, end_row: int = 20):
+  """"""
+  print(f"📊 Reading Data Response")
+  response = ctx.deps.state.data_responses.get(tool_call_id)
+  if not response:
+    valid_ids = list(ctx.deps.state.data_responses.keys())
+    return f"No successful \"get_data\" found with tool_call_id. Valid ids: {valid_ids}"
+  else:
+    return dict(
+      window = (start_row, end_row),
+      data_window = response.rows[start_row:end_row],
+      total_rows = len(response.rows),
+    )
--- agent_examples/copilotkit-chat-with-your-data/src/app/page.tsx
+++ agent_examples/copilotkit-chat-with-your-data/src/app/page.tsx
@@ -1,18 +1,11 @@
 "use client";
 
-import { ProverbsCard } from "@/components/proverbs";
-import { WeatherCard } from "@/components/weather";
-import { MoonCard } from "@/components/moon";
-import { AgentState } from "@/lib/types";
-import {
-  useCoAgent,
-  useDefaultTool,
-  useFrontendTool,
-  useHumanInTheLoop,
-  useRenderToolCall,
-} from "@copilotkit/react-core";
-import { CopilotKitCSSProperties, CopilotSidebar } from "@copilotkit/react-ui";
-import { useState } from "react";
+import {ProverbsCard} from "@/components/proverbs";
+import {WeatherCard} from "@/components/weather";
+import {AgentState} from "@/lib/types";
+import {useCoAgent, useFrontendTool, useRenderToolCall,} from "@copilotkit/react-core";
+import {CopilotKitCSSProperties, CopilotSidebar} from "@copilotkit/react-ui";
+import {useState} from "react";
 
 export default function CopilotKitPage() {
   const [themeColor, setThemeColor] = useState("#6366f1");
@@ -47,29 +40,12 @@ export default function CopilotKitPage() {
         }}
         suggestions={[
           {
-            title: "Generative UI",
-            message: "Get the weather in San Francisco.",
+            title: "Which customers have placed the highest number of orders?",
+            message: "Which customers have placed the highest number of orders?",
           },
           {
-            title: "Frontend Tools",
-            message: "Set the theme to green.",
-          },
-          {
-            title: "Human In the Loop",
-            message: "Please go to the moon.",
-          },
-          {
-            title: "Write Agent State",
-            message: "Add a proverb about AI.",
-          },
-          {
-            title: "Update Agent State",
-            message:
-              "Please remove 1 random proverb from the list if there are any.",
-          },
-          {
-            title: "Read Agent State",
-            message: "What are the proverbs?",
+            title: "What are top performing territories?",
+            message: "What are top performing territories?",
           },
         ]}
       >
@@ -83,11 +59,7 @@ function YourMainContent({ themeColor }: { themeColor: string }) {
   // 🪁 Shared State: https://docs.copilotkit.ai/pydantic-ai/shared-state
   const { state, setState } = useCoAgent<AgentState>({
     name: "my_agent",
-    initialState: {
-      proverbs: [
-        "CopilotKit may be new, but its the best thing since sliced bread.",
-      ],
-    },
+    initialState: {},
   });
 
   //🪁 Generative UI: https://docs.copilotkit.ai/pydantic-ai/generative-ui
@@ -98,20 +70,6 @@ function YourMainContent({ themeColor }: { themeColor: string }) {
       parameters: [{ name: "location", type: "string", required: true }],
       render: ({ args, result }) => {
         return <WeatherCard location={args.location} themeColor={themeColor} />;
-      },
-    },
-    [themeColor],
-  );
-
-  // 🪁 Human In the Loop: https://docs.copilotkit.ai/pydantic-ai/human-in-the-loop
-  useHumanInTheLoop(
-    {
-      name: "go_to_moon",
-      description: "Go to the moon on request.",
-      render: ({ respond, status }) => {
-        return (
-          <MoonCard themeColor={themeColor} status={status} respond={respond} />
-        );
       },
     },
     [themeColor],
